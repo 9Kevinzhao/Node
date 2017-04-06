2531e5531d1e5ca19e75bef3cf0601dde83d58cf
9Kevinzhao <9kzhao@stpatrick.org>
2017-04-06 13:57:14 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,10 +1,10 @@
 const doc=document;
 let firstdiv=doc.getElementsByClassName("pictures")
-firstdiv.addEventListener("click",function(){
+firstdiv[0].addEventListener("click",function(){
     if(firstdiv.className =="pictures"){
-        div.className="pictures2"
+        firstdiv[0].className="pictures2"
     }
     else{
-        div.className="pictures"
+        firstdiv[0].className="pictures"
     }
 })
