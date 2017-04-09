911159fe4d301b8827e3dd22def77500f644e602
9Kevinzhao <9kzhao@stpatrick.org>
2017-04-09 17:10:16 -0500

    Add files via upload

--- hover.js
+++ hover.js
@@ -1,13 +1,30 @@
-function functionName() {
 
-}
 const doc = document;
 let firstdiv =doc.querySelector("div.pictures")
-firstdiv[0].addEventListener("click",function(){
-    if(firstdiv.className =="pictures"){
-        firstdiv[0].className="pictures2"
+firstdiv.addEventListener("mouseover",function(){
+
+        firstdiv.className="pictures2"
+})
+firstdiv.addEventListener("mouseout",function(){
+    if(firstdiv.className =="pictures2"){
+        firstdiv.className="pictures"
     }
-    else{
-        firstdiv[0].className="pictures"
-    }
+})
+
+let seconddiv =doc.querySelector("div.square1")
+seconddiv.addEventListener("mouseover",function(){
+seconddiv.style.opacity= 0.8;
+})
+
+seconddiv.addEventListener("mouseout",function(){
+seconddiv.style.opacity= 1;
+})
+
+let thirddiv =doc.querySelector("div.square2")
+thirddiv.addEventListener("mouseover",function(){
+thirddiv.style.opacity= 0.8;
+})
+
+thirddiv.addEventListener("mouseout",function(){
+thirddiv.style.opacity= 1;
 })
